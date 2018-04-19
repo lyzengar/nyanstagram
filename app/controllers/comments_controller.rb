7086c67fb53436d7a0f711b46f07f4daa1f4310e
Andrew Lyzenga <andrew@Andrews-MacBook-Pro.local>
2018-04-19 15:13:52 -0700

    add edit funtionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
     before_action :authorize
+    
     def create
         @cat = Cat.find(params[:cat_id])
         @comment = Comment.new(params.require(:comment).permit(:content))
@@ -12,17 +13,26 @@ class CommentsController < ApplicationController
 
     def destroy
         @comment = Comment.find(params[:id])
+        cat = @comment.cat
         if @comment.user != current_user
-            redirect_to @cat
+            redirect_to cat_path(cat)
         else
             @comment.destroy
-            redirect_to @cat
+            redirect_to cat_path(cat)
         end
     end
 
     def edit
+        @comment = Comment.find(params[:id])
+        cat = @comment.cat
+        if @comment.user != current_user
+            redirect_to cat_path(cat)
+        end
     end
 
     def update
+        @comment = Comment.find(params[:id])
+        @comment.update_attributes(params.require(:comment).permit(:content))
+        redirect_to cat_path(@comment.cat)
     end
 end
